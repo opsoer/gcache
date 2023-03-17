7fc328a62289789e5965c9cc8561838d869e9e40
opsor <2258858408@qq.com>
2023-03-17 16:48:37 +0800

    fix singleflight_test

--- singleflight/singleflight_test.go
+++ singleflight/singleflight_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"sync"
 	"testing"
+	"time"
 )
 
 var TN = 0
@@ -20,7 +21,7 @@ func TestDoCase1(t *testing.T) {
 	for i := 0; i < 10000; i++ {
 		go func() {
 			defer wg.Done()
-			g.DoOK("key", fn)
+			g.Do("key", fn)
 		}()
 	}
 	wg.Wait()
@@ -30,61 +31,76 @@ func TestDoCase1(t *testing.T) {
 	}
 }
 
-//
-//func TestDoCase2(t *testing.T) {
-//	TN = 0
-//	var g Ones
-//	wg := sync.WaitGroup{}
-//	wg.Add(100)
-//	for i := 0; i < 100; i++ {
-//		go func() {
-//			defer wg.Done()
-//			i, _ := g.DoOK("key", func() (interface{}, error) {
-//				TN++
-//				return "bar", nil
-//			})
-//			fmt.Println(i)
-//			g.DoOK("key1", func() (interface{}, error) {
-//				TN++
-//				return "", nil
-//			})
-//		}()
-//	}
-//	wg.Wait()
-//	fmt.Println(TN)
-//	if TN != 2 {
-//		t.Error("singleFlight err")
-//	}
-//}
-//
-//func TestDoCase3(t *testing.T) {
-//	TN = 0
-//	var g Ones
-//	wg := sync.WaitGroup{}
-//	wg.Add(100)
-//	for i := 0; i < 100; i++ {
-//		go func() {
-//			defer wg.Done()
-//			g.DoOK("key", func() (interface{}, error) {
-//				TN++
-//				return "", nil
-//			})
-//		}()
-//	}
-//	time.Sleep(time.Second)
-//	wg.Add(100)
-//	for i := 0; i < 100; i++ {
-//		go func() {
-//			defer wg.Done()
-//			g.DoOK("key", func() (interface{}, error) {
-//				TN++
-//				return "", nil
-//			})
-//		}()
-//	}
-//	wg.Wait()
-//	fmt.Println(TN)
-//	if TN != 2 {
-//		t.Error("singleFlight err")
-//	}
-//}
+func TestDoCase2(t *testing.T) {
+	TN = 0
+	var g Ones
+	wg := sync.WaitGroup{}
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer wg.Done()
+			i, _ := g.Do("key", func() (interface{}, error) {
+				TN++
+				return "bar", nil
+			})
+			v := i.(string)
+			if v != "bar" {
+				t.Errorf("Expect: %v, Actual: %v", "bar", v)
+			}
+
+			i, _ = g.Do("key1", func() (interface{}, error) {
+				TN++
+				return "bar", nil
+			})
+			v = i.(string)
+			if v != "bar" {
+				t.Errorf("Expect: %v, Actual: %v", "bar", v)
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Println(TN)
+	if TN != 2 {
+		t.Error("singleFlight err")
+	}
+}
+
+func TestDoCase3(t *testing.T) {
+	TN = 0
+	var g Ones
+	wg := sync.WaitGroup{}
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer wg.Done()
+			i, _ := g.Do("key", func() (interface{}, error) {
+				TN++
+				return "bar", nil
+			})
+			v := i.(string)
+			if v != "bar" {
+				t.Errorf("Expect: %v, Actual: %v", "bar", v)
+			}
+		}()
+	}
+	time.Sleep(time.Second)
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer wg.Done()
+			i, _ := g.Do("key", func() (interface{}, error) {
+				TN++
+				return "bar", nil
+			})
+			v := i.(string)
+			if v != "bar" {
+				t.Errorf("Expect: %v, Actual: %v", "bar", v)
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Println(TN)
+	if TN != 2 {
+		t.Error("singleFlight err")
+	}
+}
